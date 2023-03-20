ed0a18f94edf8ecd106594891b0a21cbfc8d7c19
Guilherme Ivo <54378909+guilhermeivo@users.noreply.github.com>
2023-03-19 21:31:12 -0300

    update asset paths

--- app/utils/utils.js
+++ app/utils/utils.js
@@ -19,7 +19,7 @@ export function getDirectoryAssetsPath(file, typeFile) {
             break;
     }
 
-    const directorPath = window.location.origin + '/app/assets/' + file + extension
+    const directorPath = window.location.pathname + 'assets/' + file + extension
     
     return directorPath
 }
