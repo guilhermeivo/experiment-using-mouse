e25aac0a1f8050550500fede121f18768edfa6d8
Guilherme Ivo <54378909+guilhermeivo@users.noreply.github.com>
2023-03-24 23:10:47 -0300

    update

--- server/dist/Api/index.js
+++ server/dist/Api/index.js
@@ -6,11 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 require("module-alias/register");
 const Server_1 = __importDefault(require("@Infrastructure/Common/Server"));
 const routes_1 = __importDefault(require("@Api/routes"));
+const connection_1 = require("@Infrastructure/Persistence/connection");
 require('dotenv').config();
 const PORT = Number(process.env.PORT) || 8000;
 const HOSTNAME = process.env.HOSTNAME || '127.0.0.1';
 Server_1.default.init();
 Server_1.default.use(routes_1.default);
+(0, connection_1.openConnection)();
 Server_1.default.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at ${HOSTNAME}:${PORT}/`);
 });
--- server/src/Api/index.ts
+++ server/src/Api/index.ts
@@ -1,6 +1,7 @@
 import 'module-alias/register'
 import app from '@Infrastructure/Common/Server'
 import routes from '@Api/routes'
+import { openConnection } from '@Infrastructure/Persistence/connection'
 require('dotenv').config()
 
 const PORT = Number(process.env.PORT) || 8000
@@ -8,6 +9,7 @@ const HOSTNAME = process.env.HOSTNAME || '127.0.0.1'
 
 app.init()
 app.use(routes)
+openConnection()
 app.listen(PORT, HOSTNAME, () => {
     console.log(`Server running at ${ HOSTNAME }:${ PORT }/`)
 })
