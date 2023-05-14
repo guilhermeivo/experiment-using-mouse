ce59a113fcfce57e04ce0ea42b5789ea0403caf4
Guilherme Ivo <54378909+guilhermeivo@users.noreply.github.com>
2023-05-14 01:56:48 -0300

    update

--- app/src/Pages/Code/index.js
+++ app/src/Pages/Code/index.js
@@ -83,7 +83,7 @@ export default customElements.define('code-page',
         addEventsListeners() {
             const inputs = document.querySelectorAll(`.${ classes['pin'] }`)
             inputs.forEach((input, key) => {
-                input.addEventListener('change', () => {
+                input.addEventListener('input', () => {
                     if (input.value) {
                         if (key === 5) {
                             const userCode = [...inputs].map((input) => input.value).join('')
